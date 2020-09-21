a05e739ed11bab2176b187f010c4d19940ae7a75
Alberto Puritano <albi7894@gmail.com>
2020-09-21 13:44:03 +0200

    first commit

---
+++ Chinese Checkers/android/src/com/winnie/the/pooh/AndroidLauncher.java
@@ -0,0 +1,18 @@
+package com.winnie.the.pooh;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.winnie.the.pooh.Game;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		config.useCompass = false;
+		config.useAccelerometer = false;
+		initialize(new Game(), config);
+	}
+}
